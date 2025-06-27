ee514d4a02001f7ab3650580e243a83a4d1e0443
ZENG <74520775+hashtag47@users.noreply.github.com>
2025-06-27 14:27:02 +0100

    Finish this session

--- 7_camera_and_renderloop/camera_and_renderloop_starter/src/script.js
+++ 7_camera_and_renderloop/camera_and_renderloop_starter/src/script.js
@@ -1,30 +1,64 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+
+console.log(OrbitControls);
 
 // initialize the scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 
-const cubeMesh = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
-scene.add(cubeMesh)
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
-  75, 
+  35,
   window.innerWidth / window.innerHeight,
   0.1,
-  30)
-camera.position.z = 5
+  30
+);
+// const aspectRatio = window.innerWidth / window.innerHeight;
+
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   200
+// );
+camera.position.z = 5;
 
 // initialize the renderer
-const canvas = document.querySelector('canvas.threejs')
+const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+// console.log(window.devicePixelRatio);
+const maxPixelRatio = Math.min(window.devicePixelRatio, 2);
+renderer.setPixelRatio(maxPixelRatio);
+
+//Instantiate the controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+const renderloop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
